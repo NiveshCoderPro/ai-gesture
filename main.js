1772431ad354e68a89612b8e1895fbad5c9a702c
NiveshCoderPro <78587468+NiveshCoderPro@users.noreply.github.com>
2021-10-20 19:50:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload() {
+
+}
+
+function setup() {
+    canvas = createCanvas(400,300);
+    video = createCapture(VIDEO);
+    canvas.position(1000,500)
+
+    video.size(350,350)
+}
+
+function draw() {
+  
+}
